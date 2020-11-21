ea39bb7d5215689491397a55484d527455280bf4
Flairings <joejonjoyce@gmail.com>
2020-11-21 23:41:02 +0000

    1.0

--- src/me/flairings/kitpvp/events/KillRewardsEvent.java
+++ src/me/flairings/kitpvp/events/KillRewardsEvent.java
@@ -14,14 +14,14 @@ import static org.bukkit.Bukkit.getConsoleSender;
 public class KillRewardsEvent implements Listener {
     @EventHandler
     public void onDeath(PlayerDeathEvent event) {
-            Player player = event.getEntity();
-            if (player.isDead() && player.getKiller() instanceof Player) {
-                Player killer = player.getKiller();
-                killer.giveExpLevels(Main.getInstance().getConfig().getInt("kill-rewards.xp-amount"));
-                for (String commands : Main.getInstance().getConfig().getStringList("kill-rewards.commands")) {
-                    dispatchCommand(getConsoleSender(), commands.replace("{PLAYER}", killer.getName()));
+        Player player = event.getEntity();
+        if (player.isDead() && player.getKiller() instanceof Player) {
+            Player killer = player.getKiller();
+            killer.giveExpLevels(Main.getInstance().getConfig().getInt("kill-rewards.xp-amount"));
+            for (String commands : Main.getInstance().getConfig().getStringList("kill-rewards.commands")) {
+                dispatchCommand(getConsoleSender(), commands.replace("{PLAYER}", killer.getName()));
+                if (Main.getInstance().getConfig().getBoolean("kill-rewards.sound-enabled")) {
                     player.playSound(player.getLocation(), Sound.SUCCESSFUL_HIT, 2F, 1F);
-
                 }
                 killer.sendMessage(CC.translate(Main.getInstance().getConfig().getString("kill-rewards.message"))
                         .replace("{PLAYER}", "" + player.getName())
@@ -29,4 +29,4 @@ public class KillRewardsEvent implements Listener {
             }
         }
     }
-
+}
